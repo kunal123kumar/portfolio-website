20c6d5d271a0e924f1d207f02607c2189a3638bf
KUNAL KUMAR <154955493+kunal123kumar@users.noreply.github.com>
2025-08-20 01:58:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Mobile nav toggle
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
